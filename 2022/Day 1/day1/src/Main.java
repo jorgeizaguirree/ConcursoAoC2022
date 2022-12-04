1131c740333b583ce9cd60f203df170ccc3e097b
jorgeizaguirree <jorge@izaguirre.eu>
2022-12-04 13:33:18 +0100

    day1b

--- 2022/Day 1/day1/src/Main.java
+++ 2022/Day 1/day1/src/Main.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
         try {
             List<String> allLines = Files.readAllLines(Paths.get("day1.input.txt"));
@@ -26,7 +24,47 @@ public class Main {
             if (sum > max) {
                 max = sum;
             }
-            System.out.println("The result is " + max + " calories! ");
+            int max1 = max;
+            sum = 0;
+            max = 0;
+            for (String line : allLines) {
+                if (line.length() == 0) {
+                    if (sum > max) {
+                        if (sum != max1) {
+                            max = sum;
+                        }
+                    }
+                    sum = 0;
+                }else {
+                    int number = Integer.parseInt(line);
+                    sum = sum + number;
+                }
+            }
+            if (sum > max) {
+                max = sum;
+            }
+            int max2 = max;
+            sum = 0;
+            max = 0;
+            for (String line : allLines) {
+                if (line.length() == 0) {
+                    if (sum > max) {
+                        if (sum != max1 && sum != max2) {
+                            max = sum;
+                        }
+                    }
+                    sum = 0;
+                }else {
+                    int number = Integer.parseInt(line);
+                    sum = sum + number;
+                }
+            }
+            if (sum > max) {
+                max = sum;
+            }
+            int max3 = max;
+            int total = max1 + max2 + max3;
+            System.out.println("The result is " + total + " calories! ");
 
         } catch (IOException e) {
             e.printStackTrace();
