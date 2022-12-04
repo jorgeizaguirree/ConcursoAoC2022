c2bdcb6ee84861ce4933730961b6773ad6a4c25c
jorgeizaguirree <jorge@izaguirre.eu>
2022-12-04 12:32:27 +0100

    day1a

---
+++ 2022/Day 1/day1/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("day1.input.txt"));
+            int sum = 0;
+            int max = 0;
+            for (String line : allLines) {
+                if (line.length() == 0) {
+                    if (sum > max) {
+                        max = sum;
+                    }
+                    sum = 0;
+                }else {
+                    int number = Integer.parseInt(line);
+                    sum = sum + number;
+                }
+            }
+            if (sum > max) {
+                max = sum;
+            }
+            System.out.println("The result is " + max + " calories! ");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
