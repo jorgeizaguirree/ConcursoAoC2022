47a00c0b935391ce0fe98fb5989563c9accf07cf
jorgeizaguirree <jorge@izaguirre.eu>
2022-12-06 00:42:18 +0100

    day3b

--- 2022/Day 3/day 3/src/Main.java
+++ 2022/Day 3/day 3/src/Main.java
@@ -14,33 +14,48 @@ public class Main {
     }
     public static void main(String[] args) {
         try {
-            char item = ' ';
+            char item;
             int total = 0;
+            int count = 0;
+            char[] line1 = null;
+            char[] line2 = null;
+            char[] line3 = null;
             List<String> allLines = Files.readAllLines(Paths.get("day3.input.txt"));
             for (String line : allLines){
                 item = ' ';
                 char[] lineChar = StringToArray(line);
-                boolean check = false;
-                int value = 0;
-                for (int i = 0; i < lineChar.length / 2 && !check; i++){
-                    for (int pos = lineChar.length - 1; pos >= lineChar.length / 2 && !check; pos--){
-                        if (lineChar[i] == lineChar[pos]){
-                            item = lineChar[i];
-                            check = true;
+                if (count == 0) {
+                    line1 = lineChar;
+                    count++;
+                }else if(count == 1){
+                    line2 = lineChar;
+                    count++;
+                }else if (count == 2) {
+                    line3 = lineChar;
+                    System.out.println(line3);
+                    boolean check = false;
+                    for (int i = 0; i < line1.length && !check; i++) {
+                        for (int j = 0; j < line2.length && !check; j++) {
+                            for (int k = 0; k < line3.length && !check; k++) {
+                                if (line1[i] == line2[j] && line1[i] == line3[k]) {
+                                    item = line1[i];
+                                    check = true;
+                                }
+                            }
                         }
                     }
+                    count = 0;
+                    System.out.println(item);
                 }
+                int value = 0;
                 if (item >= 'a' && item <= 'z'){
                     value = item - 'a' + 1;
                 }
                 else if (item >= 'A' && item <= 'Z'){
                     value = item - 'A' + 27;
                 }
-                if (lineChar.length % 2 != 0){
-                    System.out.println("odd length");
-                }
                 total = total + value;
-                System.out.println(item + " " + value);
+                line = line + 3;
             }
             System.out.println("the sum of the priorities is " + total);
 
